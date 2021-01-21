4e10eedff26a83f5dbf01918b55dc51bcfce330f
L3o-pold <leopold.jacquot@gmail.com>
2021-01-21 17:21:08 +0000

    Deploying to gh-pages from master @ ee3a2e44479891c3977a976803e8c9390aa8a044 🚀

--- Grocery_store.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Grocery_store" tilewidth="32" tileheight="32" tilecount="784" columns="16">
- <image source="tilesets_grocery.png" width="512" height="1568"/>
-</tileset>
