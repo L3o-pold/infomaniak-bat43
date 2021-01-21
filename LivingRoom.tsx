ee3a2e44479891c3977a976803e8c9390aa8a044
Léopold Jacquot <leopold.jacquot@infomaniak.com>
2021-01-21 18:20:34 +0100

    test

--- LivingRoom.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="LivingRoom" tilewidth="32" tileheight="32" tilecount="432" columns="16">
- <image source="tilesets_livingroom.png" width="512" height="864"/>
-</tileset>
--- modern_interiors_generic.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="generic" tilewidth="32" tileheight="32" tilecount="960" columns="16">
- <image source="tilesets_generic.png" width="512" height="1920"/>
-</tileset>
